f10ce134c486941aece22b2eb44edfbdac2d10e4
davekim <davidskim4@gmail.com>
2018-01-07 00:31:56 -0600

    day 8 part 2

--- day08/main.rb
+++ day08/main.rb
@@ -1,6 +1,10 @@
+require 'set'
+
 if __FILE__ == $0
 
   registers = Hash.new { |hash, key| hash[key] = 0 }
+
+  encountered_values = Set.new
 
   file = File.readlines("input.txt").each do |line|
     mr, cmd, mv, cr, operator, cv = line.match(/(^\w+) (inc|dec) (-?\d+) if (\w+) (>|>=|<|<=|!=|==) (-?\d+)/).captures
@@ -12,9 +16,13 @@ if __FILE__ == $0
       when "dec"
         registers[mr] -= mv.to_i
       end
+      encountered_values.add(registers[mr])
     end
   end
 
   # 4567
   puts "Part 1: #{registers.values.max}"
+
+  # 5636
+  puts "Part 2: #{encountered_values.max}"
 end
